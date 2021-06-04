56ab6ca72148f716988d4c3d56b1b28c86189973
1434988286@qq.com <kisses20000222>
2021-06-04 17:53:22 +0800

    Initial commit

---
+++ src/Email.java
@@ -0,0 +1,22 @@
+public class Email implements Cloneable{
+    private Attachment attachment = null;
+    public Email(){
+        this.attachment = new Attachment();
+    }
+    public Email clone(){
+        Email clone = null;
+        try{
+            clone = (Email) super.clone();
+        }
+        catch (CloneNotSupportedException e){
+            System.out.println("Clone failure");
+        }
+        return clone;
+    }
+    public Attachment getAttachment(){
+        return this.attachment;
+    }
+    public void display(){
+        System.out.println("查看邮件！");
+    }
+}
